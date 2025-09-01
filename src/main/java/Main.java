ee1b99d01a5f81780be227da81882ca43a0b497b
skilhouse <skilhouse@gmail.com>
2025-09-01 15:09:10 +0300

    Удалил комментарии в Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,9 @@ import service.ShoppingCart;
 
 public class Main {
     public static void main(String[] args) {
-        Food meat = new Meat(5, 100); // мясо 5 кг * 100 руб
-        Food redApple = new Apple(10, 50, Colour.RED);   // красные яблоки 10 кг * 50 руб
-        Food greenApple = new Apple(8, 60, Colour.GREEN); // зелёные яблоки 8 кг * 60 руб
+        Food meat = new Meat(5, 100);
+        Food redApple = new Apple(10, 50, Colour.RED);
+        Food greenApple = new Apple(8, 60, Colour.GREEN);
 
         Food[] products = {meat, redApple, greenApple};
         ShoppingCart cart = new ShoppingCart(products);
